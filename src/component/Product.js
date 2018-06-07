815d9a0d9927cbd7b8baf298bb22ab571421fa02
Florent Heyraud <florentheyraud.pro@gmail.com>
2018-06-07 12:24:45 +0200

    second commit

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Bike from './Bike'
+//import Bike from './Bike'
 import DisplayBike from './DisplayBike';
 
 
@@ -7,28 +7,34 @@ class Product extends Component {
 
     state = {
       products: [
-        {name:'', 
-         com:'',
-        image:''
-        }
-    ],
-      test: ""
-       
+        {name: 'Fixie Hypster',
+         com: 'Voyagez la barbe au vent', 
+         image:'https://www.beastybike.com/12417-atmn_xlarge/fixie-6ku-bikes-beach-bum.jpg'},
+    
+        {name: 'Vélo Grand Voyageur',
+         com: 'Le vélo de toute la famille', 
+         image:'https://www.beastybike.com/14907-atmn_xlarge/velo-de-voyage-cinelli-hobootleg.jpg'},
+    
+         {name: 'Vélo de ville version. Femme',
+         com: 'Élégant et pratique',
+         image:'https://images.internetstores.de/products//551245/02/d3a429/Vermont_Saphire_7s_blau[500x500].jpg?forceSize=true&forceAspectRatio=true&useTrim=true&trimBorder=1.5&forceAlign=center'}
+    ]
   }
  
- renderProduct = Bike.map(product => <DisplayBike name={product.name} com={product.com} image={product.image}/>)
-
+ 
  addProduct = (event) => {
-   this.setState({ test: 'https://www.decathlon.fr/media/840/8405298/big_9ef78f58-e0da-4416-8460-5d799be9cc66.jpg' })
+ this.setState({products:[...this.state.products, { name: "Le célèbre Btwin", com:'robuste et facile à entretenir', image: 'https://www.decathlon.fr/media/840/8405298/big_9ef78f58-e0da-4416-8460-5d799be9cc66.jpg' }]}, ()=> console.log(this.state.products))
  }
     
   render() {
+  
+  const renderProduct = this.state.products.map(product => <DisplayBike name={product.name} com={product.com} image={product.image} key={product.name}/>)
+  
 
     return (      
-
         <div>   
-          {this.renderProduct}  
-          <button onClick={ this.addProduct.bind(this)}>Ajouter</button>
+          {renderProduct}  
+          <button onClick={ this.addProduct}>Ajouter</button>
           <img src={this.state.test} />
         </div>
         
